d947ecdde920ce89cdd37a4b2021d7b907d8326a
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-04 23:57:57 -0700

    setup routes for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-// import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Routes>
+          <Route path="/about" element={<About />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return (
     <div>
-      Portfolio
+      <h2>About</h2>
     </div>
   );
 }
 
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
 export default App;
