5390fdcff1f323ff408f001bc4404dfaf2aad1d2
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-05-07 03:45:47 -0700

    footer created

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "../index.css";
 
 function Footer() {
-  return <footer>This is footer</footer>;
+  return (
+    <footer>
+      <div class="flex items-center justify-center gap-6 m-5">
+        <a href="https://github.com/TigranBalayan10">
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="4x"
+            className="text-gray-600 hover:text-zinc-900"
+            target='_blank'
+          ></FontAwesomeIcon>
+        </a>
+        <a href="https://twitter.com/Tiko_102">
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size="4x"
+            className="text-gray-600 hover:text-blue-700"
+            target='_blank'
+          ></FontAwesomeIcon>
+        </a>
+        <a href="https://www.facebook.com/tigran.balayan.1">
+          <FontAwesomeIcon
+            icon={faFacebook}
+            size="4x"
+            className="text-gray-600 hover:text-blue-600"
+            target='_blank'
+          ></FontAwesomeIcon>
+        </a>
+        <a href="https://www.facebook.com/tigran.balayan.1">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="4x"
+            className="text-gray-600 hover:text-blue-400"
+            target='_blank'
+          ></FontAwesomeIcon>
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -2,19 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css"
 
+
 function Nav() {
   return (
     <nav class="flex items-center justify-between flex-wrap bg-gray-600 p-6">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          class="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
-        <span class="font-semibold text-xl tracking-tight">Tigran Balayan</span>
+        <span class="font-semibold text-lg tracking-tight">Tigran Balayan</span>
       </div>
       <ul class="flex justify-evenly gap-20 mr-10">
         <li class="mr-8">
@@ -43,27 +36,3 @@ function Nav() {
 }
 
 export default Nav;
-
-{/* <nav className="navbar navbar-light bg-info">
-  <div className="container-fluid">
-    <a className="navbar-brand">Tigran Balayan</a>
-    <form className="d-flex gap-5 me-5">
-      <Link to="/about" className="btn btn-outline-success border-0">
-        About
-      </Link>
-      <Link
-        to="/project"
-        className="btn btn-outline-success border-0"
-        id="header-link"
-      >
-        Project
-      </Link>
-      <Link to="/contact" className="btn btn-outline-success border-0">
-        Contact
-      </Link>
-      <Link to="/resume" className="btn btn-outline-success border-0">
-        Resume
-      </Link>
-    </form>
-  </div>
-</nav>; */}
